244e6860fe226ca16cf61e1d42ee035bb2a6135f
Lily Kwan <Lily@LaiYees-MacBook.local>
2016-09-15 00:55:34 -0700

    finished implementing bfs weapon logic

--- public/src/components/player.js
+++ public/src/components/player.js
@@ -1,25 +1,64 @@
 /* globals Crafty */
+const Constants = require('../constants.js');
+const mapGrid = Constants.mapGrid;
+const wallDirection = Constants.wallDirection;
 
 module.exports = function(Crafty, model) {
   Crafty.c('Player', {
     init: function() {
-      this.requires('Actor, Color, Collision')
-          .moveInDirections(2);
+      this.requires('Actor, Color, Collision, Text');
+      this.charSpeed = 2;
     },
 
-    moveInDirections(speed) {
+    bindingKeyEvents() {
       this.charMove = { left: false, right: false, up: false, down: false };
-      this.charSpeed = speed;
 
-      model.playerMoveInDirections.bind(this, speed)();
+      this.bind('EnterFrame', function() {
+        if (this.charMove.right || this.charMove.left ||
+            this.charMove.up || this.charMove.down) {
+          this.socket.emit('updatePos', {
+            playerId: this.playerId,
+            charMove: this.charMove
+          });
+        }
+      });
+
+      this.bind('KeyDown', function(e) {
+        this.charMove.left = false;
+        this.charMove.right = false;
+        this.charMove.down = false;
+        this.charMove.up = false;
+
+        if (e.keyCode === Crafty.keys.RIGHT_ARROW) this.charMove.right = true;
+        if (e.keyCode === Crafty.keys.LEFT_ARROW) this.charMove.left = true;
+        if (e.keyCode === Crafty.keys.UP_ARROW) this.charMove.up = true;
+        if (e.keyCode === Crafty.keys.DOWN_ARROW) this.charMove.down = true;
+
+        if (e.keyCode === Crafty.keys.Z) {
+          this.pickUpWeapon();
+        }
+
+        if (e.keyCode === Crafty.keys.X) {
+          this.shootWeapon();
+        }
+      });
+
+      this.bind('KeyUp', function(e) {
+        if (e.keyCode === Crafty.keys.RIGHT_ARROW) this.charMove.right = false;
+        if (e.keyCode === Crafty.keys.LEFT_ARROW) this.charMove.left = false;
+        if (e.keyCode === Crafty.keys.UP_ARROW) this.charMove.up = false;
+        if (e.keyCode === Crafty.keys.DOWN_ARROW) this.charMove.down = false;
+      });
+
       return this;
     },
 
-    setUp: function(playerId, playerColor) {
+    setUp: function(playerId, playerColor, weapons) {
       this.playerId = playerId;
       if (playerColor) {
         this.color(playerColor);
       }
+      this.existingWeapons = weapons;
       return this;
     },
 
@@ -28,7 +67,32 @@ module.exports = function(Crafty, model) {
       return this;
     },
 
-    
+    pickUpWeapon: function() {
+      this.socket.emit('pickUpWeapon', {
+        playerId: this.playerId
+      });
+
+      this.socket.on('pickUpWeapon', data => {
+        this.weaponType = data.type;
+        this.color('white');
+        // const weaponDisplay = Crafty(this.weaponDisplayId);
+        // weaponDisplay.createText(this.weaponType);
+      });
+    },
+
+    shootWeapon: function() {
+      this.socket.emit('shootWeapon', {
+        playerId: this.playerId
+      });
+    },
+
+    getCol: function() {
+      return Math.floor(this.x / mapGrid.TILE_WIDTH);
+    },
+
+    getRow: function() {
+      return Math.floor(this.y / mapGrid.TILE_HEIGHT);
+    }
   });
 
   Crafty.c('OtherPlayer', {
@@ -36,11 +100,15 @@ module.exports = function(Crafty, model) {
       this.requires('Actor, Color');
     },
 
-    setUp: function(playerId, playerColor) {
+    setUp: function(playerId, playerColor, weaponDisplayId) {
       this.playerId = playerId;
 
       if (playerColor) {
         this.color(playerColor);
+      }
+
+      if (weaponDisplayId) {
+        this.weaponDisplayId = weaponDisplayId;
       }
 
       return this;
