73a8b96aeb3d7c84a3822f8d467ea4842400ce02
Lily Kwan <lilyeekw@gmail.com>
2016-09-16 01:00:20 -0700

    fixed maze dimensions

--- public/src/components/canvas.js
+++ public/src/components/canvas.js
@@ -3,9 +3,14 @@ var createPlayerComponent = require('./player.js');
 var createWeaponComponent = require('./weapon.js');
 var createSideBarComponent = require('./sidebar.js');
 var createBallComponent = require('./ball.js');
+const Constants = require('../constants.js');
+const mapGrid = Constants.mapGrid;
 
 module.exports = function(Crafty, model) {
-  Crafty.init(500, 500);
+  const width = mapGrid.NUM_ROWS * mapGrid.TILE_WIDTH;
+  const height = mapGrid.NUM_COLS * mapGrid.TILE_HEIGHT;
+
+  Crafty.init(width, height);
 
   createComponents(Crafty, model);
   createPlayerComponent(Crafty, model);
